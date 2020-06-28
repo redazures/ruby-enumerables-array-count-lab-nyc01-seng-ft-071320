185e10302c0f4327f76beaca942a63d82c36e3d3
james wu <jwu_nyc@yahoo.com>
2020-06-28 15:11:01 +0000

    Done.

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,21 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
+  count=0
+  array.each do|element|
+    count+=1 if element.kind_of? String
+  end
+  count
 end
+
+ar=[2,3,"james","ben"," ",""]
+#puts count_strings(ar)
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
+  count=0
+  array.each do|element|
+    count+=1 if element==" " || element==""
+  end
+  count
 end
+puts count_empty_strings(ar)
