5923ad05d4b576096aa10646729f100a1bfeff9f
james wu <jwu_nyc@yahoo.com>
2020-06-28 15:08:31 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -7,7 +7,7 @@ def count_strings(array)
 end
 
 ar=[2,3,"james","ben"]
-count_strings()
+count_strings(ar)
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
